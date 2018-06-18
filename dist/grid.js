d5ef7ee23eef2c556721be59962fee87a4ac2bf8
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-18 13:25:17 -0700

    working on initial canvas

---
+++ dist/grid.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById('canvas');
+var c = canvas.getContext('2d');
+c.fillStyle = "red";
+c.fillRect(100,100,400,300); 
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  entry: './dist/entry.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
