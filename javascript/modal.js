3a5086cd8b586ef72ec5c1f148cdd423d0c3b508
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-19 21:35:25 -0700

    working on instructions modal

---
+++ javascript/modal.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let instructions = document.getElementById("modal-background");
+  let openI = document.getElementById("instructions");
+  let close = document.getElementById("close");
+
+  openI.addEventListener("click", () => {
+    instructions.style.display = "block";
+  });
+
+  close.addEventListener("click", () =>{
+    instructions.style.display = "none";
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === instructions){
+      instructions.style.display = "none";
+    }
+  });
+
+
+});
